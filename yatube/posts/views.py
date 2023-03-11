c4cd447863d4575bba4fce384ad099f5f1b27569
Николай Гиржу <ngirzhu@yandex.ru>
2023-03-11 19:49:29 +0300

    Код перенесен из шаблонов в base

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,18 +7,22 @@ number_output_records = 10
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:number_output_records]
+    title = 'Главная страница Yatube'
     context = {
         'posts': posts,
+        'title': title
     }
     return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, slug=None):
+def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:number_output_records]
     template = 'posts/group_list.html'
+    title = 'Информация групп Yatube'
     context = {
         'group': group,
         'posts': posts,
+        'title': title
     }
     return render(request, template, context)
